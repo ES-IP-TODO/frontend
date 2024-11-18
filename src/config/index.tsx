204066a518ecacb8b68e02dcdd34753d51bbfe4c
Your Name <joaquimvr15@gmail.com>
2024-11-18 16:36:24 +0000

    added coisas

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,20 +1,18 @@
 let HOST, BASE_URL, WS_SCHEME;
+
 const scheme = {
-    HTTP: 'http://',
-    HTTPS: 'https://',
+    HTTP: "http://",
+    HTTPS: "https://",
 };
-if (import.meta.env.PROD) {
-    HOST = 'load-balancer-iap-690512408.eu-north-1.elb.amazonaws.com:444';
-    BASE_URL = `${scheme.HTTPS}${HOST}`;
-} else {
-    HOST = 'localhost';
-    BASE_URL = `${scheme.HTTP}${HOST}:8000`;
-}
+
+HOST = 'load-balancer-iap-690512408.eu-north-1.elb.amazonaws.com:444';
+BASE_URL = `${scheme.HTTPS}${HOST}`;
+
 const config = {
     PRODUCTION: import.meta.env.PROD,
     HOST,
     BASE_URL,
     API_USER_URL: `${BASE_URL}/auth`,
-    API_TASK_URL: `${BASE_URL}/tasks`,
 };
+
 export default config;
